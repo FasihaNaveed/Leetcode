8fae31863f2ac3b7784671dbfe0646c7349cd062
FasihaNaveed <fasihanaveed777@gmail.com>
2025-07-04 21:21:49 +0500

    Time: 0 ms (100%), Space: 8.7 MB (96.88%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        bool seen[26] = {false};
+        for(char c : sentence)
+            seen[c - 'a'] = true;
+
+        for(bool val : seen)
+            if(!val) return false;
+
+        return true;
+    }
+};
